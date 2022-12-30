39e1998b2e8acb4b0d47ca918848ad83b254bb24
BHAVANI28122 <bhavanigavala@gmail.com>
2022-12-30 19:01:01 +0530

    do

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,12 +1,108 @@
 package com.driver;
 
+import java.util.Arrays;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
+        super(name,balance,5000);
+        this.tradeLicenseId = tradeLicenseId;
+        if(balance < getMinBalance()){
+            throw new Exception("Insufficient Balance");
+        }
     }
+     boolean check(String id)
+    {
+
+        int n = id.length();
+        char ch[] = id.toCharArray();
+        Arrays.sort(ch);
+        for (int i = 1; i < n; i++) {
+
+            if (ch[i] - ch[i - 1] != 1)
+                return false;
+        }
+
+        return true;
+    }
+
+
+    static char getMaxCountChar(int[] count)
+    {
+        int max = 0;
+        char ch = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                ch = (char)((int)'a' + i);
+            }
+        }
+        return ch;
+    }
+
+    static String rearrangeString(String S)
+    {
+
+        int N = S.length();
+        if (N == 0)
+            return "";
+
+        int[] count = new int[26];
+        for (int i = 0; i < 26; i++) {
+            count[i] = 0;
+        }
+        for (char ch : S.toCharArray()) {
+            count[(int)ch - (int)'a']++;
+        }
+
+        char ch_max = getMaxCountChar(count);
+        int maxCount = count[(int)ch_max - (int)'a'];
+
+
+        if (maxCount > (N + 1) / 2)
+            return "";
+
+        String res = "";
+        for (int i = 0; i < N; i++) {
+            res += ' ';
+        }
+
+        int ind = 0;
+
+        while (maxCount > 0) {
+            res = res.substring(0, ind) + ch_max
+                    + res.substring(ind + 1);
+            ind = ind + 2;
+            maxCount--;
+        }
+        count[(int)ch_max - (int)'a'] = 0;
+
+
+        for (int i = 0; i < 26; i++) {
+            while (count[i] > 0) {
+                ind = (ind >= N) ? 1 : ind;
+                res = res.substring(0, ind)
+                        + (char)((int)'a' + i)
+                        + res.substring(ind + 1);
+                ind += 2;
+                count[i]--;
+            }
+        }
+        return res;
+    }
+
+    // Driver's Code
+
 
     public void validateLicenseId() throws Exception {
         // A trade license Id is said to be valid if no two consecutive characters are same
@@ -14,6 +110,12 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        if(check(getTradeLicenseId())== false){
+
+            String res = rearrangeString(tradeLicenseId);
+            if (res == "")
+                throw new Exception("Valid License can not be generated");
+        }
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,28 +1,52 @@
 package com.driver;
-
+import java.lang.Math;
 public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+        super(name,balance,0);
+        this.maxWithdrawalLimit =maxWithdrawalLimit;
+        this.rate = rate;
 
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount > maxWithdrawalLimit){
+            throw new Exception("Insufficient Balance");
+        }
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
+          double Simpleinterest = getBalance()*years*rate;
+          return Simpleinterest;
+    }
 
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+        double compoundInterest = getBalance() * Math.pow((1 + rate / times), times * years);
+        return compoundInterest;
     }
 
 }
